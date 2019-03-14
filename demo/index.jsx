6c7e0ec0cf96232ab8671a3ab3842883cd8efb8c
yaoxinc <yaoxinc@yonyou.com>
2019-03-14 11:10:48 +0800

    初始化

---
+++ demo/index.jsx
@@ -0,0 +1,75 @@
+
+import { Con, Row, Col } from 'bee-layout';
+import { Panel } from 'bee-panel';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from 'bee-button';
+import './demo.scss';
+const pkg = require('../package.json')
+
+
+
+
+const CARET = <i className="uf uf-arrow-down"></i>;
+
+const CARETUP = <i className="uf uf-arrow-up"></i>;
+
+
+import Demo1 from "./demolist/Demo1";
+var DemoArray = [{"example":<Demo1 />,"title":" 树组件，带有input框","code":"/**\n *\n * @title 树组件，带有input框\n * @description 树组件，带有input框,同RefTreeWidthInput\n *\n */\n\nimport React, { Component } from 'react';\nimport RefTreeBase from './RefTreeBase';\nimport RefWithInput from 'ref-core/lib/refs/refcorewithinput.js';\nimport  'ref-core/lib/refs/refcorewithinput.css';\nimport { Button, Form } from 'tinper-bee';\nconst option = {\n    title: '树',\n    searchable: true,\n    multiple: true,\n    param: {\n        \"refCode\":\"neworganizition_tree\",\n    },\n    checkStrictly: true,\n    disabled: false,\n    nodeDisplay: (record) => {\n        return record.refname\n    },\n    displayField: (record) => {\n        return record.refname\n    },//显示内容的键\n    valueField: 'refpk',//真实 value 的键\n    refModelUrl: {\n        treeUrl:'https://mock.yonyoucloud.com/mock/358/blobRefTree',\n        treeUrl: '/pap_basedoc/common-ref/blobRefTree',\n    },\n    matchUrl: '/pap_basedoc/common-ref/matchPKRefJSON',\n    filterUrl: '/pap_basedoc/common-ref/filterRefJSON',\n    lazyModal: false,\n    strictMode: true,\n    lang: 'zh_CN'\n};\n// createRefTree({...option, showModal: true})\nclass Demo1 extends Component {\n    constructor() {\n        super();\n        this.state = {\n        }\n    }\n    render() {\n        const { getFieldProps, getFieldError } = this.props.form;\n        return (\n            <div className=\"demoPadding\">\n                <RefWithInput\n                    {...option}\n                    {...getFieldProps('code1', {\n                        initialValue: JSON.stringify({\n                            code: \"org1\",\n                            id: \"a4cf0601-51e6-4012-9967-b7a64a4b2d47\",\n                            name: \"用友集团\",\n                            refcode: \"org1\",\n                            refname: \"用友集团\",\n                            refpk: \"a4cf0601-51e6-4012-9967-b7a64a4b2d47\",\n                        }),\n                        rules: [{\n                            message: '请输入请选择', pattern: /[^{\"refname\":\"\",\"refpk\":\"\"}|{\"refpk\":\"\",\"refname\":\"\"}]/\n                        }]\n                    })}\n                >\n                 <RefTreeBase />\n                </RefWithInput>\n                \n                <Button  \n                    colors=\"primary\"\n                    onClick={() => {\n                    this.props.form.validateFields((err, values) => {\n                        console.log(err, values)\n                    });\n                }}>submit</Button>\n                <span className='error' style={{display:'block',color:'#f53c32'}}>\n                    {getFieldError('code1')}\n                </span>\n            </div>\n        )\n    }\n};\n\n\n","desc":" 树组件，带有input框,同RefTreeWidthInput"}]
+
+
+class Demo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState({ open: !this.state.open })
+    }
+
+    render () {
+        const { title, example, code, desc  } = this.props;
+        let caret = this.state.open ? CARETUP : CARET;
+        let text = this.state.open ? "隐藏代码" : "查看代码";
+
+        const footer = (
+            <Button shape="block" onClick={ this.handleClick }>
+                { caret }
+                { text }
+            </Button>
+        );
+        return (
+            <Col md={12} >
+                <h3>{ title }</h3>
+                <p>{ desc }</p>
+                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0}}>
+                    <pre><code className="hljs javascript">{ process.env.NODE_ENV==='development'?code:code.replace('../../src/index.js',pkg.name).replace('../../src/index',pkg.name) }</code></pre>
+                </Panel>
+            </Col>
+        )
+    }
+}
+
+export default class DemoGroup extends Component {
+    constructor(props){
+        super(props)
+    }
+    render () {
+        return (
+                <Row>
+                    {DemoArray.map((child,index) => {
+
+                        return (
+                            <Demo example= {child.example} title= {child.title} code= {child.code} desc= {child.desc} key= {index}/>
+                        )
+
+                    })}
+                </Row>
+        )
+    }
+}
+
---
+++ src/RefTreeBaseUI.js
@@ -0,0 +1,286 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { is } from 'immutable';
+import RefCoreError from 'ref-core/lib/refs/RefCoreError';
+import RefCoreTree from 'ref-core/lib/refs/RefCoreTree';
+import RefCoreSearch from 'ref-core/lib/refs/RefCoreSearch';
+import RefCoreButton from 'ref-core/lib/refs/RefCoreButton';
+import  'ref-core/lib/refs/refcoreerror.css';
+import  'ref-core/lib/refs/refcoretree.css';
+import  'ref-core/lib/refs/refcoresearch.css';
+import  'ref-core/lib/refs/refcorebutton.css';
+import { Loading,Modal } from 'tinper-bee'
+import './index.less';
+const noop = () => {
+};
+const propTypes = {
+  title: PropTypes.string,
+  backdrop: PropTypes.bool,
+  className: PropTypes.string,
+  showLoading: PropTypes.bool,
+  searchable: PropTypes.bool, //  是否应用搜索 默认 false,
+  valueField: PropTypes.string,
+  checkStrictly: PropTypes.bool,
+  showLine: PropTypes.bool,
+  lazyModal: PropTypes.bool,
+  showModal:PropTypes.bool,
+  lang: PropTypes.string,
+  defaultExpandAll: PropTypes.bool,  // 数默认展开
+  nodeDisplay:PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+  ]),
+  buttons:PropTypes.object,
+  emptyBut: PropTypes.bool, //清空按钮
+  getRefTreeData: PropTypes.func,
+  multiple: PropTypes.bool, //  默认单选
+  checkedArray: PropTypes.array,
+
+};
+const defaultProps = {
+  title: '弹窗标题',
+	searchable: false, //  是否应用搜索 默认 false,
+	multiple: false, //  默认单选
+	showLine: false, //  默认单选
+	defaultExpandAll: true,  // 数默认展开
+	checkStrictly: false,
+	checkedArray: [], //  指定已选择数据id
+	lazyModal: false,
+	emptyBut: false,
+	onCancel: noop,
+	onSave: noop,
+  lang: 'zh_CN',
+  nodeDisplay:'{refname}'
+}
+class RefTreeBaseUI extends Component {
+  constructor(props) {
+    super(props);
+    const { checkedArray, valueField ,expandedKeys,onSaveCheckItems,showLoading} = props;
+    this.state = {
+      selectedArray: checkedArray || [], //  记录保存的选择项
+      checkedKeys: checkedArray.map(item => {
+        return item[valueField];
+      }),
+      expandedKeys: expandedKeys,
+      onSaveCheckItems: onSaveCheckItems,
+      showLoading: showLoading
+    };
+
+    this.treeData = props.treeData;
+  }
+
+  componentWillReceiveProps(nextProps,nextState) {
+    let { checkedArray, valueField } = nextProps;
+    if(!is(nextState, this.state)){
+      this.treeData = nextProps.treeData;
+      this.setState({
+        selectedArray: checkedArray || [], //  记录保存的选择项
+        checkedKeys: checkedArray.map(item => {
+          return item[valueField];
+        }),
+      })
+    }
+    
+  }
+  //  tree EventHandler
+	onCheck(selectedKeys, event) {
+		const { multiple } = this.props;
+		if(!multiple){
+			//单选
+			this.setState({
+				selectedArray: [event.node.props.attr],
+				checkedKeys: [event.node.props.eventKey],
+				onSaveCheckItems: [event.node.props.attr]
+			});
+		}else{
+			let { valueField } = this.props;
+			let allProcessCheckedArray = [].concat(this.state.selectedArray);
+			let key = event.node.props.attr[valueField];
+			let currentNode = event.node.props.attr;
+			if (event.checked) {
+				//新增操作
+				allProcessCheckedArray.push(currentNode)
+			} else {
+				//删除操作
+				allProcessCheckedArray = allProcessCheckedArray.filter(item=>{
+				  return item[valueField] !== key
+				})
+			}
+			this.setState({
+				selectedArray: allProcessCheckedArray,
+				checkedKeys: selectedKeys,
+				onSaveCheckItems: allProcessCheckedArray,
+			});
+
+		}
+	}
+	onDoubleClick(selectedKeys, event) {
+		
+		const item = event.node.props;
+		const arr = [{ ...item.attr, refpk: item.eventKey, id: item.eventKey }]
+		this.setState({
+			selectedArray: arr,
+			checkedKeys: [item.eventKey]
+		}, () => {
+			this.onClickBtn('save')
+		})
+	}
+
+	onSelect(selectedKeys, event) {
+		const { checkAllChildren, multiple } = this.props;
+		const eventKey = event.node.props.eventKey
+		let { onSaveCheckItems } = this.state
+		let ishaskey = false
+		let keyIndex;
+		if(multiple) return;
+		onSaveCheckItems.forEach((v, i) => {
+			if (v.id == eventKey) {
+				keyIndex = i
+			}
+			if (v.id == eventKey && v.checkAllchildren) {
+				ishaskey = true
+				return false
+			}
+		})
+		if (ishaskey) {
+			this.setState({
+				checkedKeys: selectedKeys,
+			});
+			return false
+		}
+		if (!checkAllChildren) {
+			const arr = event.selectedNodes.map((item) => {
+				return { ...item.props.attr, refpk: item.key, id: item.key }
+			})
+			this.setState({
+				selectedArray: arr,
+				checkedKeys: selectedKeys,
+			});
+		} else {
+			let arr = {}
+			event.selectedNodes.forEach((item) => {
+				if (item.key == eventKey) {
+					arr = { ...item.props.attr, refname: item.props.title, refpk: item.key, id: item.key }
+				}
+			})
+			if (selectedKeys.indexOf(eventKey) > -1) {
+				onSaveCheckItems.push(arr)
+			} else {
+
+				onSaveCheckItems.splice(keyIndex, 1)
+			}
+
+			this.setState({
+				selectedArray: onSaveCheckItems,
+				checkedKeys: selectedKeys,
+				onSaveCheckItems: onSaveCheckItems
+			});
+		}
+	}
+  onSearchClick = (value) => {
+		this.props.getRefTreeData(value);
+	};
+	onSearchChange = (value) => {
+		this.props.getRefTreeData(value);
+	};
+
+  onClickBtn = (type) => {
+    const { onCancel, onSave } = this.props;
+    switch (type) {
+      case 'save':
+        onSave(this.state.selectedArray);
+        break;
+      case 'cancel':
+        this.setState({
+          selectedArray: [],
+          checkedKeys: [],
+          onSaveCheckItems: [],//20190124不保存那么选中的数据清空
+        }, () => {
+          onCancel();
+        });
+        break;
+      case 'clear':
+        this.setState({
+          selectedArray: [],
+          checkedKeys: [],
+        }, () => {
+        });
+        break;
+      default:
+        this.setState({ selectedArray: [] }, () => {
+        });
+    }
+  };
+  render() {
+    let {
+      title,
+      backdrop,
+      className,
+      showLoading,
+      searchable,
+      valueField,
+      checkStrictly,
+      showLine,
+      lazyModal,
+      showModal,
+      lang,
+      defaultExpandAll,
+      nodeDisplay = "{refname}",
+      buttons,
+      emptyBut,
+      multiple
+    } = this.props;
+    const { checkedKeys } = this.state;
+    if(checkedKeys.length === 0) emptyBut = false; //20190226没有选中数据清空按钮不展示
+    return (
+      <Modal
+        show={showModal}
+        size="sm"
+        className={`${className} ref-core  ref-core-modal ref-tree`}
+        backdrop={backdrop}
+        onHide={() => this.onClickBtn('cancel')}
+      >
+        <Loading show={showLoading} showBackDrop={true} loadingType="line" displayType={"block"} />
+          <Modal.Header closeButton>
+            <Modal.Title > {title} </Modal.Title>
+          </Modal.Header >
+
+          <Modal.Body>
+            <RefCoreSearch
+              show={searchable}
+              onSearch={this.onSearchClick}
+              onChange={this.onSearchChange}
+              language={lang}
+            />
+            {
+              this.treeData.length ?
+                <RefCoreTree
+                  show={Boolean(this.treeData.length)}
+                  nodeKeys={(item) => item[valueField]}
+                  displayField={nodeDisplay}
+                  data={this.treeData}
+                  defaultExpandAll={lazyModal ? false : defaultExpandAll}
+                  checkable={multiple}
+                  multiple={multiple}
+                  onCheck={this.onCheck.bind(this)}
+                  onSelect={this.onSelect.bind(this)}
+                  onDoubleClick={this.onDoubleClick.bind(this)}
+                  checkedKeys={checkedKeys}
+                  selectedKeys={checkedKeys}
+                  checkStrictly={checkStrictly}
+                  showLine={showLine}
+                  loadData={lazyModal ? this.onLoadData.bind(this) : null}
+                /> :
+                <RefCoreError show={!Boolean(this.treeData.length)} language={lang} />
+            }
+          </Modal.Body>
+          <Modal.Footer className={'ref-core-modal-footer'}>
+            <RefCoreButton language={lang} onClickBtn={this.onClickBtn} buttons={buttons} emptyBut={emptyBut} />
+          </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+RefTreeBaseUI.propTypes = propTypes;
+RefTreeBaseUI.defaultProps = defaultProps;
+export default RefTreeBaseUI;
