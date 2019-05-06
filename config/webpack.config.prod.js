c64045667db19a5b0ecc86f3afe53ccf933c8292
yaoxinc <yaoxinc@yonyou.com>
2019-05-06 15:03:53 +0800

    修改

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -11,9 +11,10 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const baseConfig = require('./webpack.base')
 module.exports = webpackMerge(baseConfig, {
   mode:'production',
-  entry: {
-      app: path.join(__dirname, '../src/index.js'),
-  },
+  entry: [
+    path.join(__dirname, '../src/index.js'),
+    path.join(__dirname, '../src/index.less'),
+  ],
   externals:['react','react-dom','prop-types','tinper-bee','ref-core'],
   output: {
       filename: 'index.js',
